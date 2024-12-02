f1b9fc07db3afc45590f96ae6a0224a23cbaadd5
Amber <110812972+Msambere@users.noreply.github.com>
2024-12-02 16:23:01 -0600

    Add clickability to increase and decrease Temp
    Also added an inital temp of 70 to HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+
+// Wave 2
+const state = {
+  tempValue: 70,
+}
+
+const increaseTemp = () =>{
+  state.tempValue += 1;
+  const temp = document.getElementById('tempValue');
+  temp.textContent = state.tempValue;
+}
+
+const decreaseTemp = () =>{
+  state.tempValue -= 1;
+  const temp = document.getElementById('tempValue');
+  temp.textContent = state.tempValue;
+}
+
+const registerEventHandlers = () => {
+  const increaseTempButton = document.getElementById("increaseTempControl");
+  increaseTempButton.addEventListener("click", increaseTemp);
+  
+  const decreaseTempButton = document.getElementById("decreaseTempControl");
+  decreaseTempButton.addEventListener("click", decreaseTemp);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
