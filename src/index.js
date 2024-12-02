08f5e089cafab99ad9a3b419c3ca5f7f5b2e6a52
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-12-02 14:26:24 -0800

    completed wave 3 - created an element that displays the city name typed into text input bar, set "Seattle" as default city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+// Wave 3
+document.addEventListener("DOMContentLoaded", () => {
+    const headerCityName = document.getElementById("headerCityName");
+    const cityNameInput = document.getElementById("cityNameInput");
+    const cityNameReset = document.getElementById("cityNameReset");
+
+    // Update city name when user types city in the input field
+    cityNameInput.addEventListener("input", () => {
+        headerCityName.textContent = cityNameInput.value;
+    });
+
+    // When user clicks on reset the city name will default to Seattle
+    cityNameReset.addEventListener("click", () => {
+        headerCityName.textContent = "Seattle";
+        cityNameInput.value = "";
+    });
+});
