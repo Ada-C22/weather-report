80c10c3624907f44897193633d1f5bc6812af63a
shiyinxi <shiyinxi@gmail.com>
2024-12-02 14:53:02 -0800

    add increase/decrease button event handler

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 "use strict";
 
+const state = {
+    curTemp : 60,
+}
+
+const increaseTemp = () => {
+    const temp = document.getElementById("tempValue");
+    state.curTemp += 1;
+    temp.textContent = state.curTemp;
+};
+
+const decreaseTemp = () => {
+    const temp = document.getElementById("tempValue");
+    state.curTemp -= 1;
+    temp.textContent = state.curTemp;
+};
+
+const registerEventHandlers= () => {
+    const increaseButton = document.getElementById("increaseTempControl");
+    increaseButton.addEventListener("click", increaseTemp)
+
+    const decreaseButton = document.getElementById("decreaseTempControl");
+    decreaseButton.addEventListener("click", decreaseTemp)
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
