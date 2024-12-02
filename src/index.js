460130110bd80564efe8af5bab7a82cddffb51be
Amber <110812972+Msambere@users.noreply.github.com>
2024-12-02 16:53:10 -0600

    Change temp text color depending on value

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 "use strict";
 
+// Constants
+
+
+
 
 // Wave 2
 const state = {
   tempValue: 70,
+  tempValueColor: 'orange'
 }
 
 const increaseTemp = () =>{
   state.tempValue += 1;
   const temp = document.getElementById('tempValue');
   temp.textContent = state.tempValue;
-}
+  manageTempValueColor();
+};
 
 const decreaseTemp = () =>{
   state.tempValue -= 1;
   const temp = document.getElementById('tempValue');
   temp.textContent = state.tempValue;
+  manageTempValueColor();
+};
+
+const manageTempValueColor = () =>{
+  const temp = document.getElementById('tempValue').textContent;
+  if (temp >= 80){
+    state.tempValueColor = 'red';
+  }else if (temp <=79 && temp >=70){
+    state.tempValueColor = 'orange';
+  }else if (temp <=69 && temp>=60){
+    state.tempValueColor = 'yellow';
+  }else if (temp <=59 && temp>=50){
+    state.tempValueColor = 'green';
+  }else if (temp <=49){
+    state.tempValueColor = 'teal';
+  }
+
+  document.getElementById('tempValue').style.color = state.tempValueColor;
 }
+
 
 const registerEventHandlers = () => {
   const increaseTempButton = document.getElementById("increaseTempControl");
@@ -26,4 +51,8 @@ const registerEventHandlers = () => {
   decreaseTempButton.addEventListener("click", decreaseTemp);
 };
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener("DOMContentLoaded", ()=>{
+  registerEventHandlers();
+  manageTempValueColor();
+
+});
