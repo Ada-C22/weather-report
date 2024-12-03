364311172ad64f4b2d83235108967e48735cc981
mayboolean <>
2024-12-02 19:38:22 -0500

    implemented 2 clickable elements, created respective functions, registered events to +/= temp on click. Text color and landscape will change according to temp

---
+++ src/index.js
@@ -0,0 +1,65 @@
+const state = {
+    temperature: 69,
+};
+
+// Function when clicking on up button
+const increaseTemp = () => {
+    const currentTemp = document.getElementById("tempValue");
+    state.temperature += 1;
+    currentTemp.textContent = state.temperature;
+    changeTextColor(currentTemp)
+    changeLandscape()
+};
+
+// Function when clicking on down button
+const decreaseTemp = () => {
+    const currentTemp = document.getElementById("tempValue");
+    state.temperature -= 1;
+    currentTemp.textContent = state.temperature;
+    changeTextColor(currentTemp)
+    changeLandscape()
+};
+
+const changeTextColor = (currentTemp) => {
+    if (state.temperature > 80) {
+        currentTemp.style.color = "red";
+    } else if (state.temperature >= 70) {
+        currentTemp.style.color = "orange";
+    } else if (state.temperature >= 60) {
+        currentTemp.style.color = "yellow";
+    } else if (state.temperature >= 50) {
+        currentTemp.style.color = "green";
+    } else if (state.temperature <= 49) {
+        currentTemp.style.color = "teal";
+    }
+};
+
+const changeLandscape = () => {
+    currentLandscape = document.getElementById("landscape")
+    if (state.temperature >= 80) {
+        currentLandscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (state.temperature >= 70) {
+        currentLandscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (state.temperature >= 60) {
+        currentLandscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if (state.temperature <= 59) {
+        currentLandscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    }
+}
+
+
+// register the buttons and their respective listener + function
+const registerEventHandlers = () => {
+    // when DOM loads, the default temp text should also change colors
+    const currentTemp = document.getElementById("tempValue");
+    changeTextColor(currentTemp)
+    changeLandscape()
+    
+    const upButton = document.querySelector("#increaseTempControl");
+    upButton.addEventListener("click", increaseTemp);
+
+    const downButton = document.querySelector("#decreaseTempControl")
+    downButton.addEventListener("click", decreaseTemp)
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers)
