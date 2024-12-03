f4f4715019c2c7362b205b3d53a2c16cb1669dc9
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2024-12-03 12:39:46 -0500

    Add background color and the landscape feature

--- src/index.js
+++ src/index.js
@@ -1,39 +1,89 @@
+'use strict';
+
 const state = {
-  clickCount: 100,
-    // downClickCount: 100,
-    // upClickCount: 100,
-  clicked: false
-}
-
-
-
-const updateDownClickCount = () => {
-    const downClickContainer = document.getElementById('tempValue');
-    // state.downClickCount -= 1;
-    state.clickCount -= 1;
-    downClickContainer.textContent = state.clickCount;
-    // downClickContainer.textContent = state.downClickCount;
-}
-
-const updateUpClickCount = () => {
-  const upClickContainer = document.getElementById("tempValue");
-  // state.upClickCount += 1;
-  state.clickCount += 1;
-  upClickContainer.textContent = state.clickCount;
-  // upClickContainer.textContent = state.upClickCount;
-  const temperature__section = document.getElementById("temp__title");
-  if (clickCount > 99) {
-    temperature__section.style.backgroundColor = "red";
-  }
-
+  tempCount: 70,
 };
+
+const updateDecreaseTempCount = () => {
+  const tempValue = document.getElementById('tempValue');
+  --state.tempCount;
+  tempValue.textContent = state.tempCount;
+
+  updateTempBackgroundColor();
+  updateLandscape();
+};
+
+const updateIncreaseTempCount = () => {
+  const tempValue = document.getElementById('tempValue');
+  ++state.tempCount;
+  tempValue.textContent = state.tempCount;
+  const landscape = document.getElementById('landscape');
+
+  updateTempBackgroundColor();
+  updateLandscape();
+};
+
+const updateTempBackgroundColor = () => {
+  const temperature__section = document.querySelector('.temperature__section');
+
+  switch (true) {
+    default:
+      temperature__section.style.backgroundColor = 'white';
+      break;
+    case state.tempCount >= 80:
+      temperature__section.style.backgroundColor = 'red';
+      break;
+    case state.tempCount >= 70:
+      temperature__section.style.backgroundColor = 'orange';
+      break;
+    case state.tempCount >= 60:
+      temperature__section.style.backgroundColor = 'yellow';
+      break;
+    case state.tempCount >= 50:
+      temperature__section.style.backgroundColor = 'green';
+      break;
+    case state.tempCount < 50:
+      temperature__section.style.backgroundColor = 'teal';
+      break;
+  }
+};
+
+const updateLandscape = () => {
+  const newLandscape = document.createElement('div');
+  const landscapeContainer = document.getElementById('landscape');
+  landscapeContainer.textContent =  ''
+  
+    switch (true) {
+      default:
+        newLandscape.textContent = 'test';
+        break;
+      case state.tempCount >= 80:
+        newLandscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+        break;
+      case state.tempCount >= 70:
+        newLandscape.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+        break;
+      case state.tempCount >= 60:
+        newLandscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+        break;
+      case state.tempCount < 60:
+        newLandscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+        break;
+    }
+
+    landscapeContainer.appendChild(newLandscape);
+
+}
 
 const registerEventHandlers = () => {
-  const decreaseTemp = document.getElementById('decreaseTempControl')
-  decreaseTemp.addEventListener("click", updateDownClickCount);
+  // loadControls();
+  const decreaseTemp = document.getElementById('decreaseTempControl');
+  const increaseTemp = document.getElementById('increaseTempControl');
+  decreaseTemp.addEventListener('click', updateDecreaseTempCount);
+  increaseTemp.addEventListener('click', updateIncreaseTempCount);
 
-  const increaseTemp = document.getElementById('increaseTempControl')
-  increaseTemp.addEventListener("click", updateUpClickCount);
+  updateTempBackgroundColor();
+  updateLandscape();
 };
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
