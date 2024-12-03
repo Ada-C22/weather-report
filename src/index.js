df5bdea866bcba7b4ce6349ba53e5a326f9d55c6
AstryBrana <astry.brana@gmail.com>
2024-12-02 20:14:40 -0500

    changed bg based on temp, working on wave 2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ let temperature = 70;
 function updateDisplay() {
     const tempDisplay = document.getElementById('tempValue');
     tempDisplay.textContent = `${temperature}°F`;
+    if (temperature >= 80) {
+        tempDisplay.style.backgroundColor = "red";
+    }
+    else if (temperature >= 70 && temperature < 80) {
+        tempDisplay.style.backgroundColor = "orange";
+    }
+    else if (temperature >= 60 && temperature < 70) {
+        tempDisplay.style.backgroundColor = "yellow";
+    }
+    else if (temperature >= 50 && temperature < 60) {
+        tempDisplay.style.backgroundColor = "green";
+    }
+    else if (temperature <= 49) {
+        tempDisplay.style.backgroundColor = "teal";
+    }
 };
 
 increaseTempButton.addEventListener( 'click',() => { 
@@ -17,6 +32,9 @@ decreaseTempButton.addEventListener( 'click',() => {
     temperature--;
     updateDisplay();
 });
+
+
+
 
 updateDisplay();
 
