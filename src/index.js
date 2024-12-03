2844cc2463dfc080e521ac33090b511221d88a4b
mayboolean <>
2024-12-03 14:30:51 -0500

    refactors wave 2 and 3. Added helper function changeTemp() in increaseTemp&decreaseTemp, consolidated changeTextColor and changeLandscape, added cityName state and changes in updateCityName

--- src/index.js
+++ src/index.js
@@ -1,74 +1,74 @@
-const currentTemp = document.getElementById("tempValue");
-const currentLandscape = document.getElementById("landscape")
-const searchBar = document.getElementById("cityNameInput")
 
 const state = {
     temperature: 69,
+    cityName: "",
 };
 
 // Function when clicking on up button
 const increaseTemp = () => {
     state.temperature += 1;
-    currentTemp.textContent = state.temperature;
-    changeTextColor(currentTemp)
-    changeLandscape()
+    changeTemp()
 };
 
 // Function when clicking on down button
 const decreaseTemp = () => {
     state.temperature -= 1;
+    changeTemp()
+};
+
+const changeTemp = () => {
+    const currentTemp = document.getElementById("tempValue");
     currentTemp.textContent = state.temperature;
-    changeTextColor(currentTemp)
-    changeLandscape()
+    changeStyleToTemp(currentTemp)
 };
 
-// helper function to change text color
-const changeTextColor = (currentTemp) => {
-    if (state.temperature > 80) {
-        currentTemp.style.color = "red";
-    } else if (state.temperature >= 70) {
-        currentTemp.style.color = "orange";
-    } else if (state.temperature >= 60) {
-        currentTemp.style.color = "yellow";
-    } else if (state.temperature >= 50) {
-        currentTemp.style.color = "green";
-    } else if (state.temperature <= 49) {
-        currentTemp.style.color = "teal";
-    }
-};
-
-// helper function to change landscape
-const changeLandscape = () => {
+// helper function to change text color and landscape
+const changeStyleToTemp = (currentTemp) => {
+    const currentLandscape = document.getElementById("landscape")
     if (state.temperature >= 80) {
+        currentTemp.style.color = "red";
         currentLandscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
     } else if (state.temperature >= 70) {
+        currentTemp.style.color = "orange";
         currentLandscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
     } else if (state.temperature >= 60) {
+        currentTemp.style.color = "yellow";
         currentLandscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-    } else if (state.temperature <= 59) {
+    } else if (state.temperature >= 50) {
+        currentTemp.style.color = "green";
+        currentLandscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else if (state.temperature <= 49) {
+        currentTemp.style.color = "teal";
         currentLandscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
     }
-}
+};
+
 
 // function that runs when user inputs in search bar
 const updateCityName = () => {
+    const searchBar = document.getElementById("cityNameInput")
     const cityNameDisplay = document.getElementById("headerCityName");
-    cityNameDisplay.textContent = searchBar.value;
+    state.cityName = searchBar.value;
+    cityNameDisplay.textContent = state.cityName;
 };
+
 
 // register the buttons and their respective listener + function
 const registerEventHandlers = () => {
     // when DOM loads, the default temp text should also change colors
-    changeTextColor(currentTemp)
-    changeLandscape()
+    changeTemp()
     
     const upButton = document.querySelector("#increaseTempControl");
     upButton.addEventListener("click", increaseTemp);
 
-    const downButton = document.querySelector("#decreaseTempControl")
-    downButton.addEventListener("click", decreaseTemp)
+    const downButton = document.querySelector("#decreaseTempControl");
+    downButton.addEventListener("click", decreaseTemp);
 
-    searchBar.addEventListener("input", updateCityName)
+    const searchBar = document.getElementById("cityNameInput")
+    searchBar.addEventListener("input", updateCityName);
+
+    const currentTempButton = document.querySelector("#currentTempButton");
+    // currentTempButton.addEventListener("click", getCurrentTemp)
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers)
