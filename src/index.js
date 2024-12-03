dfe6738ed6a4d3d834bd4a5aa140e483b425784e
Salma Anany <salmayousry5@gmail.com>
2024-12-02 23:48:43 -0800

    wave 3 is completed

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const syncCityName =  () => {
+    const cityNameHeader = document.getElementById('headerCityName');
+    const cityNameTextBox = document.getElementById('cityNameInput');
+    cityNameTextBox.addEventListener("input", (event) => {
+        cityNameHeader.textContent = event.target.value;
+    });
+}
+
+
+
+syncCityName();
