02e81f8ea9b0e74b59aeb7bb2e4b9d76b99bc88d
vlada <vladarap88@gmail.com>
2024-12-04 11:05:07 -0800

    finished wave 2, worked with live share

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-
 const state = {
-  currentTemp: 0,
+  currentTemp: 72,
 };
 
 // Function to update the temperature display and apply color changes
 const updateTemperatureDisplay = () => {
   const tempValueElement = document.getElementById("tempValue");
+  const landscapeElement = document.getElementById("landscape");
   tempValueElement.textContent = `${state.currentTemp} °F`;
+
+  if (state.currentTemp >= 80) {
+    tempValueElement.style.color = "red";
+    landscapeElement.textContent = "🌵  🐍 🦂 🌵🌵  🐍 🏜 🦂";
+  } else if (state.currentTemp >= 70) {
+    tempValueElement.style.color = "orange";
+    landscapeElement.textContent = "🌸🌿🌼  🌷🌻🌿 ☘️🌱 🌻🌷";
+  } else if (state.currentTemp >= 60) {
+    tempValueElement.style.color = "salmon";
+    landscapeElement.textContent = "🌾🌾 🍃 🪨  🛤 🌾🌾🌾 🍃";
+  } else if (state.currentTemp >= 50) {
+    tempValueElement.style.color = "green";
+    landscapeElement.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  } else if (state.currentTemp <= 49) {
+    tempValueElement.style.color = "blue";
+    landscapeElement.textContent = "⛄️ ⛄️ ⛄️";
+  }
 };
+
+// Function to update the temperature Color and Landscape
+
+// const designTemp = () => {
+//   const tempValueElement = document.getElementById("tempValue");
+//   const landscape = document.getElementById("landscape");
+//   tempValueElement.textContent = `${state.currentTemp} °F`;
+
+//   if (state.currentTemp >= 80) {
+//     tempValueElement.style.color = "red";
+//   } else if (state.currentTemp >= 70) {
+//     tempValueElement.style.color = "orange";
+//   } else if (state.currentTemp >= 70) {
+//     tempValueElement.style.color = "orange";
+//   }
+
+// };
 
 // Event handler to increase temperature
 const increaseTemp = () => {
