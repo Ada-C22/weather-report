4ad1b0be7623c4b16d8a1a5bce90377ab2170947
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2024-12-04 16:22:47 -0500

    Edit the coordinate function to return an object, and update the state

--- src/index.js
+++ src/index.js
@@ -1,51 +1,11 @@
 'use strict';
+// F = 1.8*(K-273) + 32.
+//  F = 9/5(K - 273) + 32
+// C = K - 273.15
 
 const state = {
   tempCount: 70,
-  cityName: null,
-};
-
-const findLatitudeAndLongitude = () => {
-  let latitude, longitude;
-  axios
-    .get("http://127.0.0.1:5000/location", {
-      params: {
-        q: state.cityName,
-        format: "json",
-      },
-    })
-    .then((response) => {
-      latitude = response.data[0].lat;
-      longitude = response.data[0].lon;
-      findLocation(latitude, longitude);
-      console.log("success in findLatitudeAndLongitude!", latitude, longitude);
-    })
-    .catch((error) => {
-      console.log("error in findLatitudeAndLongitude!");
-    });
-
-  // return {
-  //   seattleLat: latitude,
-  //   seattleLon: longitude,
-  // };
-};
-
-const findLocation = (latitude, longitude) => {
-  axios
-    .get("http://127.0.0.1:5000/weather", {
-      params: {
-        format: "json",
-        lat: latitude,
-        lon: longitude,
-      },
-    })
-    .then((response) => {
-      console.log("success in findLocation!", response.data);
-      return response.data;
-    })
-    .catch((error) => {
-      console.log("error in findLocation!");
-    });
+  cityName: "seattle",
 };
 
 const updateDecreaseTempCount = () => {
@@ -109,13 +69,14 @@ const updateLandscape = () => {
     }
 
     landscapeContainer.appendChild(newLandscape);
-}
+};
 
 const updateCityNameInput = () => {
   const cityNameInput = document.getElementById("cityNameInput");
   const headerCityName = document.getElementById("headerCityName");
   headerCityName.textContent = cityNameInput.value;
   state.cityName = cityNameInput.value;
+  console.log(state.cityName);
 };
 
 const resetCityNameInput = () => {
@@ -124,13 +85,79 @@ const resetCityNameInput = () => {
   headerCityName.textContent = "";
 };
 
+const findTemperature = (coord) => {
+  return axios
+    .get("http://127.0.0.1:5000/weather", {
+      params: {
+        lat: coord[latitude],
+        lon: coord[longitude],
+      },
+    })
+    .then((response) => {
+      console.log("success in findTemperature!", response.data.main.temp);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log("error in findTemperature!");
+    });
+};
+
+
+const findLatitudeAndLongitude = () => {
+  let latitude, longitude;
+  return axios
+    .get("http://127.0.0.1:5000/location", {
+      params: {
+        q: state.cityName,
+        format: "json",
+      },
+    })
+    .then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      console.log("success in findLatitudeAndLongitude!", latitude, longitude);
+      return {
+        lat: latitude, 
+        lon: longitude 
+      }
+    })
+    .catch((error) => {
+      console.log("error in findLatitudeAndLongitude!");
+    });
+
+  // return {
+  //   seattleLat: latitude,
+  //   seattleLon: longitude,
+  // };
+};
+
+const getTemperature = () => {
+  const tempValue = document.getElementById("tempValue");
+
+  return findLatitudeAndLongitude()
+  .then(coord => {
+    state.tempCount = 40
+    tempValue.textContent = state.tempCount;
+    const tempData = findTemperature(coord);
+    console.log(tempData)
+    return tempData;
+  });
+};
+
+  // getTemperature()
+  //   .then(temp => {
+  //     console.log(temp);
+  //   });
+
 const registerEventHandlers = () => {
   const decreaseTemp = document.getElementById('decreaseTempControl');
   const increaseTemp = document.getElementById('increaseTempControl');
+  const currentTempButton = document.getElementById("currentTempButton");
   decreaseTemp.addEventListener('click', updateDecreaseTempCount);
   increaseTemp.addEventListener('click', updateIncreaseTempCount);
   cityNameInput.addEventListener("input", updateCityNameInput);
   cityNameReset.addEventListener("click", resetCityNameInput);
+  currentTempButton.addEventListener("click", getTemperature);
 
   updateTempColor();
   updateLandscape();
